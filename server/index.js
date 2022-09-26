9fde932378ccb44d1e3af8d137e7b4cc1747a2c9
Fahad Imran <fahad.imran@devsinc.com>
2022-09-26 16:14:40 +0500

    feat: configure node server with express

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Listening on port ${PORT}...`);
+});
